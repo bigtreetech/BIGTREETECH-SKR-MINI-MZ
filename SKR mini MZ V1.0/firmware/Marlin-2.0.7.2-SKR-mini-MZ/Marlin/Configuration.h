2715afd2d1291e1cdfd3108f52ea8bf61d4331b7
Msq001 <alansayyeah@gmail.com>
2020-12-09 11:18:10 +0800

    disable speaker for Fan

--- SKR mini MZ V1.0/firmware/Marlin-2.0.7.2-SKR-mini-MZ/Marlin/Configuration.h
+++ SKR mini MZ V1.0/firmware/Marlin-2.0.7.2-SKR-mini-MZ/Marlin/Configuration.h
@@ -1807,7 +1807,7 @@
 // If you have a speaker that can produce tones, enable it here.
 // By default Marlin assumes you have a buzzer with a fixed frequency.
 //
-#define SPEAKER
+//#define SPEAKER
 
 //
 // The duration and frequency for the UI feedback sound.
@@ -1816,8 +1816,8 @@
 // Note: Test audio output with the G-Code:
 //  M300 S<frequency Hz> P<duration ms>
 //
-#define LCD_FEEDBACK_FREQUENCY_DURATION_MS 20
-#define LCD_FEEDBACK_FREQUENCY_HZ 1000
+//#define LCD_FEEDBACK_FREQUENCY_DURATION_MS 20
+//#define LCD_FEEDBACK_FREQUENCY_HZ 1000
 
 //=============================================================================
 //======================== LCD / Controller Selection =========================
